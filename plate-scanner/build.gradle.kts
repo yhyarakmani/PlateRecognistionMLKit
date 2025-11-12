602bd6ee4dadfbd3e060b0a07e8cdb943f25209c
yhyarakmani <yhya.rakmani.1997@gmail.com>
2025-11-12 23:53:55 +0300

    build: Bump version to 1.0.6
    Bumps the library version from 1.0.5 to 1.0.6.

--- plate-scanner/build.gradle.kts
+++ plate-scanner/build.gradle.kts
@@ -92,7 +92,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.cashin"
                 artifactId = "plate-scanner"
-                version = "1.0.5"
+                version = "1.0.6"
             }
         }
         repositories {
